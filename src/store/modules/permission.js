bf5d143fabe58265a10bce332520783fe0c413c8
chuns <chunsi.yang@gmail.com>
2020-09-11 21:27:52 +1000

    init update

---
+++ src/store/modules/permission.js
@@ -0,0 +1,52 @@
+import {adminRoutes, constantRoutes} from '@/router'
+import Layout from "@/layout/index";
+import {getLanguage} from "@/lang";
+
+/**
+ * Use meta.role to determine if the current user has permission
+ * @param roles
+ * @param route
+ */
+function hasPermission(roles, route) {
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+function getAdminRouter() {
+  return adminRoutes
+}
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({commit}, roles) {
+    return new Promise(resolve => {
+      let accessedRoutes = []
+      if (roles.includes('admin')) {
+        accessedRoutes = accessedRoutes.concat(getAdminRouter())
+      }
+      // 404 page must be placed at the end !!!
+      accessedRoutes.push({ path: '*', redirect: '/404', hidden: true })
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
